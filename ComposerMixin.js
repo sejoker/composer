a0e852e575f4e5af29cd710bcb20fece422f31f0
Eldar Djafarov <djkojb@gmail.com>
2014-01-08 10:31:58 +0000

    Update composer to work without reactiveness

--- ComposerMixin.js
+++ ComposerMixin.js
@@ -4,17 +4,6 @@ module.exports = {
     composer(this);
   },
   insert: function(){
-    if(arguments[1].getDOMNode){
-      this.watch(arguments[0])
-        .on('new', this.forceUpdate)
-        .on('update', function(constr){
-          constr.forceUpdate();
-        });
-    } else {
-      this.watch(arguments[0])
-        .on('new', this.forceUpdate)
-        .on('update', this.forceUpdate);
-    }
     return this.compose.apply(this, arguments);
   }
 }
--- composer.js
+++ composer.js
@@ -1,10 +1,7 @@
-var Emitter = require('./libs/emitter');
-
-// instance: 
-// params:
 var cache = {};
 var toString = Object.prototype.toString;
 
+// refactor to drop dependencies
 var isEqual = _.isEqual;
 
 function dispose(inst){
@@ -13,20 +10,19 @@ function dispose(inst){
 
 module.exports = function(app){
   app = app || {};
-  app = Emitter(app);
+  if(app.compose) return app;
+  
   app.compose = function(name, constr, params, options){
     if(arguments.length == 0) return cache;
-    if(arguments.length == 1) return cache[name];
-
+    if(arguments.length == 1) return cache[name] && cache[name].inst;
     if(params && toString.call(params) !== '[object Array]') params = [params];
-    
     // if already exists
     if(cache[name]){
       var prevConstr = cache[name].constr;
       var prevParams = cache[name].params;
       var prevInst = cache[name].inst;
       //if constr is a react class
-      if(constr.getDOMNode || toString.call(constr) == '[object Function]'){
+      if(toString.call(constr) == '[object Function]'){
         //different classes
         if(constr !== prevConstr){
           dispose.call(this, prevInst);
@@ -36,71 +32,42 @@ module.exports = function(app){
             params: params
           }
           // trigger new
-          app.emit(name + ':new');
           return cache[name].inst;
         }
         //if params are the same return same component
         if(isEqual(cache[name].params, params)){
           // not changed
           return cache[name].inst;
-        } 
-      }
-      if(constr.getDOMNode){
-        cache[name].inst.replaceProps(params[0]);
-        // trigger after update
-        cache[name].params = params;
-        app.emit(name + ':update', constr);
-        return cache[name].inst;
-      }
-      console.log(1);
-      if(toString.call(constr) == '[object Function]'){
+        }
         cache[name].inst = constr.apply(this, params);
         // trigger after update
         cache[name].params = params;
-        app.emit(name + ':update', this); 
         return cache[name].inst;
       }
+
       if(constr == Object(constr) || toString.call(constr) == '[object String]'){
         if(isEqual(cache[name].constr, constr)){
           return cache[name].constr;
         }
         cache[name].constr = constr;
         // trigger after update
-        app.emit(name + ':update');
         return constr;
       }
     } else {
-      if(constr.getDOMNode || toString.call(constr) == '[object Function]'){
+      if(toString.call(constr) == '[object Function]'){
         cache[name] = {
           constr: constr,
           inst: constr.apply(this, params),
           params: params
         }
-        console.log('new ' + name);
-        app.emit(name + ':new');
         return cache[name].inst;
       }
       if(constr == Object(constr) || toString.call(constr) == '[object String]'){
         // trigger new
         cache[name].inst = constr;
-        app.emit(name + ':new');
         return constr;
       }
     }
-  }
-  app.watch = function(name){
-    var watch =  {
-      on: function(action, callback){
-        app.on(name + ":" + action, callback);
-        return watch
-
-      },
-      off: function(action, callback){
-        app.off(name + ":" + action, callback);
-        return watch;
-      }
-    }
-    return watch;
   }
   return app;
 }
